fe07eb5c6c1333a0bded1895dd6c872ee64c4931
krzysztoffila <umowy.fila@gmail.com>
2024-07-11 13:59:15 +0200

    Feature: Add slider - fetch image, styles

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 "use strict";
 // Email Validation
-const btnNewsletter = document.querySelector(".newsletter__button");
+const btnNewsletter = document.querySelector(".newsletter__input--button");
 const inputEmail = document.querySelector(".newsletter_email-input");
 const chekboxEmail = document.querySelector(".newsletter__checkbox--check");
+const slider = document.querySelector(".slider");
+const slide_1 = document.querySelector(".slide_1");
 
 btnNewsletter.addEventListener("click", (e) => {
   e.preventDefault();
@@ -17,3 +19,27 @@ btnNewsletter.addEventListener("click", (e) => {
     inputEmail.value = "";
   }
 });
+
+// Fetch images
+async function getImages() {
+  const url = "https://picsum.photos/v2/list?page=2&limit=10";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.log(error.message);
+    return [];
+  }
+}
+
+(async () => {
+  const images = await getImages();
+  const firstImage = images[0];
+  document.querySelector(
+    ".slider"
+  ).style.backgroundImage = `url(${firstImage.download_url})`;
+})();
